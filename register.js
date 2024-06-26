abc752498a4d2966e74339745b8245e16bb72e4e
JakubIvacek <xivacek@stuba.sk>
2024-06-26 17:57:13 +0200

    updates

--- register.js
+++ register.js
@@ -1,23 +1,13 @@
-function togglePasswordVisibility() {
-    const passwordField = document.getElementById('password');
-    if (passwordField.type === 'password') {
-        passwordField.type = 'text';
-        document.getElementById('password-toggle').src = "img/hidden.png"
-    } else {
-        passwordField.type = 'password';
-        document.getElementById('password-toggle').src = "img/eye.png"
-    }
-}
+const togglePassword = document.getElementById('togglePassword');
+togglePassword
+
 function togglePasswordVisibilityRegister() {
     const passwordField = document.getElementById('password-toggle2');
-    const passwordField2 = document.getElementById('password-toggle3');
     if (passwordField.type === 'password') {
         passwordField.type = 'text';
-        passwordField2.type = 'text';
-        document.getElementById('password-togg').src = "img/hidden.png"
+        togglePassword.className = "fas fa-eye-slash toggle-password";
     } else {
         passwordField.type = 'password';
-        passwordField2.type = 'password';
-        document.getElementById('password-togg').src = "img/eye.png"
+        togglePassword.className = "fas fa-eye toggle-password";
     }
 }
