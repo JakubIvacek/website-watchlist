0e32d323a9b635284a1ff0fece73cfcd9d0fad42
JakubIvacek <xivacek@stuba.sk>
2024-09-08 00:49:10 +0200

    password toggle oprava

--- register.js
+++ register.js
@@ -1,13 +1,20 @@
 const togglePassword = document.getElementById('togglePassword');
-togglePassword
+togglePassword.addEventListener('click', togglePasswordVisibilityRegister);
 
 function togglePasswordVisibilityRegister() {
     const passwordField = document.getElementById('password-toggle2');
+    const passwordField2 = document.getElementById('password-toggle3');
     if (passwordField.type === 'password') {
         passwordField.type = 'text';
-        togglePassword.className = "fas fa-eye-slash toggle-password";
+        togglePassword.className = "fas fa-eye-slash toggle-password icon-toggle";
+        if(passwordField2 != null){
+            passwordField2.type = 'text';
+        }
     } else {
         passwordField.type = 'password';
-        togglePassword.className = "fas fa-eye toggle-password";
+        togglePassword.className = "fas fa-eye toggle-password icon-toggle";
+        if(passwordField2 != null){
+            passwordField2.type = 'password';
+        }
     }
 }
